0be7de115b3aa487c1a2ca0d1b158d95e0fff973
q408901310 <408901310@qq.com>
2022-06-12 23:18:16 +0800

    main

--- index.php
+++ index.php
@@ -1,38 +1,6 @@
 <?php
-echo 'aaa';exit;
 $proxys = [
-    '145.239.85.58:9300',
-    '46.4.96.137:1080',
-    '47.91.88.100:1080',
-    '45.77.56.114:30205',
-    '82.196.11.105:1080',
-    '51.254.69.243:3128',
-    '178.62.193.19:1080',
-    '188.226.141.127:1080',
-    '217.23.6.40:1080',
-    '185.153.198.226:32498',
-    '81.171.24.199:3128',
-    '5.189.224.84:10000',
-    '108.61.175.7:31802',
-    '176.31.200.104:3128',
-    '83.77.118.53:17171',
-    '173.192.21.89:80',
-    '163.172.182.164:3128',
-    '163.172.168.124:3128',
-    '164.68.105.235:3128',
-    '5.199.171.227:3128',
-    '93.171.164.251:8080',
-    '212.112.97.27:3128',
-    '51.68.207.81:80',
-    '91.211.245.176:8080',
-    '84.201.254.47:3128',
-    '95.156.82.35:3128',
-    '185.118.141.254:808',
-    '164.68.98.169:9300',
-    '217.113.122.142:3128',
-    '188.100.212.208:21129',
-    '83.77.118.53:17171',
-    '83.79.50.233:64527'
+
 ];
 
 
@@ -47,6 +15,22 @@ function em_getallheaders()
         }
     }
     return $headers;
+}
+
+
+function send($url, $method = 'GET', $params = [], $headers = [], $options = [])
+{
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+    if ($method == 'POST') {
+        $options[CURLOPT_POST] = TRUE;
+        $options[CURLOPT_POSTFIELDS] = $params;
+    }
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt_array($ch, $options);
+    $result = curl_exec($ch);
+    return curl_errno($ch) ? curl_error($ch) : $result;
 }
 
 function multi($urls, $cookie, $headers = [], $options = [], $retry = 1)
@@ -138,7 +122,7 @@ if(!$rspBody) {
     echo json_encode(['code' => 1001, 'data' => 'not response body']);
     exit;
 }
-echo $rspBody;exit;
+
 $rspHeader = explode("\r\n", $rspHeader);
 foreach ($rspHeader as $item) {
     if(strpos($item, ':') === false) continue;
@@ -166,11 +150,10 @@ $rsqHeaders = [
     "group" => $group,
     "host:apilb.stepn.com",
 ];
-$urls = array_slice($urls, 0, 5);
+//$urls = array_slice($urls, 0, 5);
 $vals = [];
-$listUrls = array_chunk($urls, 5);
-foreach ($listUrls as $listUrl) {
-    $vals = array_merge($vals, multi($listUrl, $cookie));
+foreach ($urls as $url) {
+    $vals[] = send($url, 'GET', null, $rsqHeaders);
 }
 $resultDict = [];
 
